838fc8260742c391a4f037f21ccd39c38ae24932
Dmitrijs Pavlovs <dmitrijs@hebsdigital.com>
2019-03-18 11:00:02 +0200

    Update

--- gulp-tasks/hebs-modular.js
+++ gulp-tasks/hebs-modular.js
@@ -1,87 +1,16 @@
 const gulp = require('gulp');
 const flatmap = require('gulp-flatmap');
 const minimist = require("minimist");
-const assetsJson = require('../custom-gulp-plugins/assets-json');
-const sourcemaps = require('gulp-sourcemaps');
-const gulpIf = require('gulp-if');
-const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 const print = require('gulp-print').default;
-const minimatch = require("minimatch")
+
 const colors = require('ansi-colors');
 const path = require("path")
-const order = require('gulp-order');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
+
 const configLoader = require('../common/config-loader');
 const log = require('fancy-log');
 const notifier = require('node-notifier');
-const babel = require('gulp-babel');
-const tap = require('gulp-tap')
-const { spawn } = require('child_process');
 
-// module.exports = function() {
-
-//     /* Set environment variable from cli flag*/
-//     let env;
-//     const args = minimist(process.argv.slice(2));
-    
-//     switch (args.env) {
-//         case "default":
-//             env = "default"
-//             break;
-    
-//         case "development":
-//             env = "development"
-//             break;
-    
-//         case "production":
-//             env = "production"
-//             break;
-    
-//         default:
-//             env = "default"
-//             break;
-//     }
-//         configLoader.loadSkin(args.skinpath)
-        
-//         return gulp.src(args.skinpath)
-    
-//             /* Creates a stream for each skin */
-//             .pipe(flatmap(function(stream, skinDir) {
-              
-    
-//                 return gulp.src([`${skinDir.path}/**/${paths.src.sourceFolder}`, `!**/node_modules/**/*`, `!**/master/**/*`, `!**/${paths.dist.outputFolder}/**/*`])
-    
-//                     /* Creates a stream for each scripts folder*/
-//                     .pipe(flatmap(function(stream, dir) {
-//                       log(process.cwd())
-//                       log(dir.path)
-//                     return gulp.src([`${dir.path}/**/scripts.js`,`!${dir.path}/**/*${paths.src.ignoreSuffix}*`],{allowEmpty: true })
-
-//                         .pipe(print(filepath => `Stream started for: ${colors.unstyle(path.dirname(filepath))}/${colors.red(path.basename(filepath))}`))
-//                         .pipe(gulpIf(run[env].script.babel, babel(options[env].script.babel)))
-//                         .pipe(tap(function(file) {
-//                           log("Testo")
-//                           spawn(`./node_modules/.bin/webpack --config=${args.skinpath}/webpack.config.js --mode=none --context=${args.skinpath}`, {stdio: 'inherit', shell: true});
-//                         }))
-                        
-//                         .pipe(gulp.dest(file=>file.base))
-//                         .pipe(print(filePath => `Finished '(${colors.cyan(args.skindir)}) stream for file:  ${colors.unstyle(path.dirname(filePath))}/${colors.blue(path.basename(filePath))}'`))
-        
-    
-//                     }))
-//             }))
-//             .on('end', function() {
-    
-//                 notifier.notify({
-//                     title: 'Hebspack',
-//                     message: `Scripts bundled in ${args.skindir}`,
-//                     icon: args.iconpath,
-//                 });
-    
-//             })
-//     }
 
 
 module.exports = function() {
@@ -106,7 +35,7 @@ module.exports = function() {
                     return gulp.src([`${dir.path}/**/scripts.js`,`!${dir.path}/**/*${paths.src.ignoreSuffix}*`],{allowEmpty: true })
 
                         .pipe(print(filepath => `Stream started for: ${colors.unstyle(path.dirname(filepath))}/${colors.red(path.basename(filepath))}`))
-                        // .pipe(gulpIf(run[env].script.babel, babel(options[env].script.babel)))
+
 
                         
                         .pipe(webpackStream({
@@ -167,70 +96,4 @@ module.exports = function() {
             })
     }
 
-
-
-
-// module.exports = 
-
-// function() {
-//     /* Set environment variable from cli flag*/
-//     let env;
-//     const args = minimist(process.argv.slice(2));
-    
-//     switch (args.env) {
-//         case "default":
-//             env = "default"
-//             break;
-    
-//         case "development":
-//             env = "development"
-//             break;
-    
-//         case "production":
-//             env = "production"
-//             break;
-    
-//         default:
-//             env = "default"
-//             break;
-//     }
-
-//     configLoader.loadSkin(args.skinpath)
-
-
-//   return new Promise(resolve => webpack({
-
-//     entry: `${args.skinpath}/assets/scripts.js`,
-//     output: {
-//         filename: `scripts.min.js`,
-//         path:  `${args.skinpath}/dist/`,
-//     },
-//     module: {
-//         rules: [
-//           {
-//             test: /\.(js)$/,
-//             loader: 'babel-loader',
-//             // options: {
-//             //   presets: [ 
-//             //     [ "@babel/preset-env", {
-//             //       useBuiltIns: 'entry',
-//             //       modules: "auto"
-//             //     }]
-//             //   ],
-//             //   plugins: ['syntax-dynamic-import']
-//             // }
-//           }
-//         ]
-//       },
-
-//   }, (err, stats) => {
-
-//       if (err) console.log('Webpack', err)
-
-//       console.log(stats.toString({ /* stats options */ }))
-
-//       resolve()
-
-//   }))
-// } 
 
